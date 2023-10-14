ecb76855418a69f8201847320be0bde7d38ece1d
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2023-10-13 22:37:09 -0400

    Update 5_Financial Dash - Part 3 - Clients.py

--- pages/5_Financial Dash - Part 3 - Clients.py
+++ pages/5_Financial Dash - Part 3 - Clients.py
@@ -17,7 +17,7 @@ with st.expander("See app info"):
 # Load your financial dataset
 df = pd.read_excel('chatGPTDemo.xlsx')
 # Assuming your dataset is loaded into a pandas DataFrame named 'df'
-df['dollar_volume'] = df['quantity'] * df['Close'] #code carried over from Part 2
+df['dollar_volume'] = df['quantity'] * df['Adj Close'] #code carried over from Part 2
 
 st.title("Financial Data Dashboard")
 
